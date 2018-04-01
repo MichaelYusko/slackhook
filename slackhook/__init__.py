f1e2d882fbe276c38c746fa0a1f16a85b5c2509a
MichaelYusko <mykhaylo.yusko@gmail.com>
2018-04-01 17:11:33 +0300

    Upload on pypi

--- slackhook/__init__.py
+++ slackhook/__init__.py
@@ -1,45 +1,8 @@
-from flask import request
-import collections
-
+from .hook import SlackWebHook
 
 __author__ = 'Michael Yusko'
 __email__ = 'freshjelly12@yahoo.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 __all__ = ['SlackWebHook']
-
-
-class SlackWebHook:
-    """Build a slack-webhook for a flask application
-
-    :param app An flask application
-    :param endpoint Name for a endpoint
-    """
-    def __init__(self, app, endpoint='/slack-webhook'):
-        app.add_url_rule(rule=endpoint, endpoint=endpoint,
-                         view_func=self._receive_text, methods=['POST'])
-
-        self._webhooks = collections.defaultdict(list)
-        self._webhook_types = ['out-going-hook']
-
-    def hook(self, hook_type='out-going-hook'):
-        """Register an functions as hook
-
-        :param hook_type A name of a hook
-        """
-        def wrapper(func):
-            self._webhooks[hook_type].append(func)
-            print(self._webhooks)
-            return func
-        return wrapper
-
-    def _receive_text(self):
-        """Callback from a Flask application"""
-        data = request.form.get('text', '')
-        for type_ in self._webhook_types:
-            if type_ in self._webhooks.keys():
-                for hook in self._webhooks.get(type_):
-                    return hook(data)
-            else:
-                raise ValueError('Hook type {} not allowed'.format(type_))
